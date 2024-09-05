6a6596b9a033095c21ce611ff76e2c662f5e4818
arjunmoola <arjunmoola123@gmail.com>
2024-09-05 10:46:44 -0500

    updated feed follows table to include created_at and updated_at columns

--- sql/schema/004_feed_follows.sql
+++ sql/schema/004_feed_follows.sql
@@ -1,6 +1,8 @@
 -- +goose Up
 CREATE TABLE feed_follows (
     id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     user_id UUID REFERENCES users ON DELETE CASCADE NOT NULL,
     feed_id UUID REFERENCES feeds ON DELETE CASCADE NOT NULL
 );
